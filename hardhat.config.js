b34be2a48b7f485e10851c45845d63dd34945cbb
Pozzitron1337 <the.vaho1337@gmail.com>
2022-09-23 12:45:54 +0300

    added networks

--- hardhat.config.js
+++ hardhat.config.js
@@ -32,24 +32,44 @@ module.exports = {
       },
       allowUnlimitedContractSize: false,
       timeout: 9999999999,
-      blockGasLimit: 1000_000_000,
+      blockGasLimit: 1_000_000_000,
       gas: 100_000_000,
       gasPrice: 30_000_000_000,
       accounts: {mnemonic: MNEMONIC}
     },
     private: {
-      url: `https://192.`,
+      url: `https://192.168.0.1:8545`,
       gas: 100_000_000,
       gasPrice: 90_000_000_000,
       accounts: {mnemonic: MNEMONIC}
     },
     bsctest: {
       url: 'https://data-seed-prebsc-1-s1.binance.org:8545',
-      gas: 2_000_000,
-      gasPrice: 50_000_000_000,
+      gas: 3_000_000,
+      gasPrice: 10_000_000_000,
+      blockGasLimit: 1_000_000_000,
+      timeout: 99999999,
+      accounts: {mnemonic: MNEMONIC}
+    },
+    avalanchetest: {
+      url: 'https://api.avax-test.network/ext/bc/C/rpc',
+      gas: 3_000_000,
+      gasPrice: 100_000_000_000,
+      blockGasLimit: 1_000_000_000,
+      timeout: 99999999,
+      accounts: {mnemonic: MNEMONIC}
+    },
+    mumbai: {
+      url: 'https://matic-mumbai.chainstacklabs.com',
+      gas: 3_000_000,
+      gasPrice: 100_000_000_000,
+      blockGasLimit: 1_000_000_000,
       timeout: 99999999,
       accounts: {mnemonic: MNEMONIC}
     }
+  },
+  etherscan: {
+    apiKey: ETHERSCAN_API_KEY,
   },
   contractSizer: {
     alphaSort: true,
